607947cd9c7213038ababdd701e1a3964479c760
chetan1904singh <chetan1904singh@gmail.com>
2026-07-09 17:37:21 +0530

    added frontend

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,7 @@ app.use(express.json());
 
 app.use("/auth", userRoutes);
 
-mongoose
-    .connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log("MongoDB Connected");
 
---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import Game from "./pages/Game";
+import Result from "./pages/Result";
+
+function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<Login />} />
+
+            <Route path="/signup" element={<Signup />} />
+
+            <Route path="/home" element={<Home />} />
+
+            <Route path="/game" element={<Game />} />
+
+            <Route path="/result" element={<Result />} />
+        </Routes>
+    );
+}
+
+export default App;
