2fcf70b058426c5a2014b72c506795237fe003fc
chetan1904singh <chetan1904singh@gmail.com>
2026-08-14 01:04:58 +0530

    Deployed on atlas

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,8 @@ app.use("/auth", userRoutes);
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+         origin: "*",
+        methods: ["GET", "POST"]
     },
 });
 
